4f9b80bf117a28b790dbe6f1b8651ccfe79e1a0a
Multus <Tima.jet@yandex.ru>
2022-11-16 16:44:01 +0300

    const links added

--- rdec2d.hpp
+++ rdec2d.hpp
@@ -12,6 +12,39 @@ struct Rpol2D{
 };
 
 
+Rdec2D operator+(const Rdec2D& lhs, const Rdec2D& rhs);
+Rdec2D operator-(const Rdec2D& vec);
+Rdec2D operator-(const Rdec2D& lhs, const Rdec2D& rhs);
+Rdec2D operator*(const Rdec2D& vec, const double& sca);
+Rdec2D operator*(const double& sca, const Rdec2D& vec);
+double dot(const Rdec2D& lhs, const Rdec2D& rhs);
+double norm(const Rdec2D& vec);
+
+Rpol2D pol(const Rdec2D& vec);
+Rdec2D dec(const Rpol2D& vec);
+
+Rpol2D operator+(const Rpol2D& lhs, const Rpol2D& rhs);
+Rpol2D operator-(const Rpol2D& vec);
+Rpol2D operator-(const Rpol2D& lhs, const Rpol2D& rhs);
+Rpol2D operator*(const Rpol2D& vec, const double& sca);
+Rpol2D operator*(const double& sca, const Rpol2D& vec);
+double dot(const Rpol2D& lhsp, const Rdec2D& rhsp);
+double norm(const Rpol2D& vec);
+
+/* #pragma once
+
+#include <Rdec2D>
+
+struct Rdec2D{
+	double x = 0.0;
+	double y = 0.0;
+};
+struct Rpol2D{
+	double r = 0.0;
+	double theta = tau;
+};
+
+
 Rdec2D operator+(Rdec2D lhs, Rdec2D rhs);
 Rdec2D operator-(Rdec2D vec);
 Rdec2D operator-(Rdec2D lhs, Rdec2D rhs);
@@ -30,3 +63,4 @@ Rpol2D operator*(Rpol2D vec, double sca);
 Rpol2D operator*(double sca, Rpol2D vec);
 double dot(Rpol2D lhs, Rdec2D rhs);
 double norm(Rpol2D vec);
+*/
