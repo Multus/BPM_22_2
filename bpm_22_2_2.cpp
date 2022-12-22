c212864e12dce2705143c5c523c4362ba55e754e
Multus <Tima.jet@yandex.ru>
2022-12-22 14:09:26 +0300

    Update bpm_22_2_2.cpp
    А, нет, надо было просто while(std::cin)…

--- bpm_22_2_2.cpp
+++ bpm_22_2_2.cpp
@@ -7,18 +7,13 @@ int main(){
     double count_nice = 0;
     double x = 0;
     double y = 0;
-    while(true){
-        try{
-            std::cin >> x >> y;
-            count_all += 1;
-            if(y >= std::fabs(x) && y <= 2 - std::pow(x,2)){
-                count_nice += 1;
-            }
-        }
-        catch (const std::runtime_error& e){
-            std::cout << count_nice / count_all;
-            return 0;
+    while(std::cin){
+        std::cin >> x >> y;
+        count_all += 1;
+        if(y >= std::fabs(x) && y <= 2 - std::pow(x,2)){
+            count_nice += 1;
         }
     }
+    std::cout << count_nice / count_all;
+    return 0;
 }
-
